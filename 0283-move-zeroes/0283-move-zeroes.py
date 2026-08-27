19f2e4813ba3a9cb424b1ccf728643d8ff2a1c30
Mubashshir <mohammedmubashshiraliadeeb@gmail.com>
2026-08-27 16:38:54 +0530

    Time: 739 ms (6.09%), Space: 13.3 MB (88.87%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        zerocount=nums.count(0)
+        while 0 in nums:
+            nums.remove(0)
+        nums.extend([0]*zerocount)
